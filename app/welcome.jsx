1c25731d2ad996cef2957846c1b371ac3eeeb05e
Ting <baiyuting652@gmail.com>
2025-02-08 13:02:21 +1100

    add button loading component

---
+++ app/welcome.jsx
@@ -0,0 +1,67 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '../components/ScreenWrapper'
+import { StatusBar } from 'expo-status-bar'
+import { hp, wp } from '../helpers/common'
+import { theme } from '../constants/theme'
+import Button from '../components/Button'
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        {/* Welcome Image */}
+        <Image style={styles.welcomeImage} resizeMode='contain' source={require('../assets/images/welcome.png')} />
+
+        {/* title */}
+        <View style={{ gap: 20 }}>
+            <Text style={styles.title}>Let's Social!</Text>
+            <Text style={styles.punchline}>Connect in Campus</Text>
+        </View>
+
+        {/* footer */}
+        <View style={styles.footer}>
+            <Button 
+                title="Getting Started"
+                buttonStyle={{ marginHorizontal: wp(3) }}
+                onPress={() => {}}
+            />
+        </View>
+      </View>
+    </ScreenWrapper>
+  )
+}
+
+export default Welcome
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: 'white',
+        marginHorizontal: wp(10)
+    },
+    welcomeImage: {
+        height: hp(30),
+        width: wp(100),
+        alignSelf: 'center',
+    },
+    title: {
+        color: theme.colors.text,
+        fontSize: hp(4),
+        textAlign: 'center',
+        fontWeight: theme.fonts.extrabold,
+    },
+    punchline: {
+        textAlign: 'center',
+        paddingHorizontal: wp(10),
+        fontSize: hp(1.7),
+        color: theme.colors.text
+    },
+    footer: {
+        gap: 30,
+        width: "100%"
+    }
+})
